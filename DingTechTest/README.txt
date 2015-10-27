3da791f1d06671e899772f3a1c13dfb9d2fada46
BrianJVarley <brianvarley9@gmail.com>
2015-10-27 21:52:22 +0000

    Initial Commit

---
+++ DingTechTest/README.txt
@@ -0,0 +1,28 @@
+﻿README - Ding Graduate .NET Developer – Technical Test Solutions
+
+Developer - Brian J Varley
+
+================================================================
+
+*Part 1:
+
+See console application, "XMLTopUpParser"
+
+*Part 2:
+
+Transaction response 1, shows four occurences of a failure code 99 in the response byte array.
+Transaction response 2, shows four occurences of a failure code 99 in the response byte array.
+
+
+*Part 3:
+
+API integration questions - 
+
+1.How active is the API's customer support service, and what level of support is offered?
+2.How is the API's latency and uptime viewed?
+3.Is there a .Net client library for the API?
+4.How are API, errors logged and monitored?
+5.Is there additional documentation on the API?
+6.What other platforms has the API been integrated and developed for?
+7.Are there any usage/traffic limits with the API?
+8.How active is the API in daily usage, ie, how many calls are made to the API daily?
---
+++ DingTechTest/XMLTopUpParser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DingTechTest
+{
+
+    public class XMLTopUpParser
+    {
+
+        private static byte[] requestBytes;
+        private static string fileContents;
+        private static string responseXML;
+
+
+        static void Main(string[] args)
+        {
+
+	        Console.WriteLine("Enter start/exit:"); // Prompt
+	        string line = Console.ReadLine(); // Get string from user
+	        if (line == "exit") // Check string
+	        {
+                Environment.Exit(0);
+	        }
+            else
+            {
+
+                fileContents = File.ReadAllText(@"request.xml");
+                ParseXML(fileContents);
+
+                Console.WriteLine("Converting request to response object..");
+
+                responseXML = System.Text.Encoding.UTF8.GetString(requestBytes);
+            
+            }
+            
+        }
+
+
+
+        public static byte[] ParseXML(string xml)
+        {
+
+            try
+            {
+                requestBytes = System.Text.Encoding.UTF8.GetBytes(fileContents);
+                Console.WriteLine("request parsing successful!");
+                Console.WriteLine("...........................");
+                Console.ReadKey(true);
+                return (requestBytes);
+                
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: ", e);
+
+            }
+        }
+
+
+    }
+}
